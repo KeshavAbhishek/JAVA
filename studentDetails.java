04a7f4119fc5499f45401ebe6a2b17edb62e0b88
Keshav Abhishek <crystaled2003@gmail.com>
2024-01-18 23:44:30 +0530

    Commit

---
+++ studentDetails.java
@@ -0,0 +1,9 @@
+public class studentDetails {
+    public static void main(String[] args) {
+        long regNumber = 2201020582l;
+        String name = "KESHAV ABHISHEK";
+        int group = 6;
+        float cgpa = 8.5f;
+        System.out.print("Name : " + name + "\nRegistration Number : " + regNumber + "\nGroup : " + group + "\nCGPA : " + cgpa);
+    }
+}
